ffb050d53e18eecba869ffd51708edfdec40d002
Shota Yonezawa <s.yone101@gmail.com>
2012-04-24 07:13:59 +0900

    delete win.js

--- Resources/win.js
+++
@@ -1,8 +0,0 @@
-var win = Ti.UI.currentWindow;
-
-var view = Ti.UI.createView({
-    top:40, left:20, right:20, height:280,
-    borderColor:'black',
-    backgroundColor:'orange'
-});
-win.add(view);
